678f4f71c95818c7c582ad25a9c430247fb36c9e
OlgaLyalyueva <olgalyalyueva@gmail.com>
2021-04-23 10:20:19 +0300

    Add Meta classes and __str__ function to models.py

--- Garage/models.py
+++ Garage/models.py
@@ -36,15 +36,36 @@ class Car(models.Model):
     price = models.IntegerField(blank=True, null=True, verbose_name='Стоимость')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.producer + ', ' + self.model
+
+    class Meta:
+        verbose_name = 'Автомобиль'
+        verbose_name_plural = 'Автомобили'
+
 
 class Body(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
     name = models.CharField(blank=True, max_length=100, verbose_name='Тип кузова')
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тип кузова'
+        verbose_name_plural = 'Типы кузовов'
+
 
 class Engine(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
     name = models.CharField(blank=True, max_length=500, verbose_name='Тип двигателя')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Двигатель'
+        verbose_name_plural = 'Типы двигателей'
 
 
 class Insurance(models.Model):
@@ -57,24 +78,45 @@ class Insurance(models.Model):
     price = models.IntegerField(verbose_name='Стоимость страховки')
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.type + ', ' + self.car_id)
+
+    class Meta:
+        verbose_name = 'Страховка'
+        verbose_name_plural = 'Страховки'
+
 
 class CarProblem(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
     date = models.DateField(auto_now=True, blank=True)
     name = models.CharField(max_length=255, verbose_name='Название проблемы')
-    description = models.CharField(blank=True, max_length=1000, verbose_name='Описание')
+    description = models.CharField(blank=True, null=True, max_length=1000, verbose_name='Описание')
     state = models.BooleanField(default=True, verbose_name='Состояние')
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + ', ' + self.car_id
+
+    class Meta:
+        verbose_name = 'Тип неполадки'
+        verbose_name_plural = 'Типы неполадок'
 
 
 class Improvement(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
     date = models.DateField(auto_now=True, blank=True)
     name = models.CharField(max_length=255, verbose_name='Название проблемы')
-    description = models.CharField(blank=True, max_length=1000, verbose_name='Описание')
+    description = models.CharField(blank=True, null=True, max_length=1000, verbose_name='Описание')
     state = models.BooleanField(default=True, verbose_name='Состояние')
     price = models.IntegerField(blank=True, null=True, verbose_name='Стоимость')
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + ', ' + self.car_id
+
+    class Meta:
+        verbose_name = 'Улучшение'
+        verbose_name_plural = 'Улучшения'
 
 
 class Repair(models.Model):
@@ -85,9 +127,16 @@ class Repair(models.Model):
         (3, 'Кузовные работы')
     ])
     name = models.CharField(max_length=400, verbose_name='Название')
-    description = models.CharField(blank=True, max_length=2000, verbose_name='Описание')
-    note = models.CharField(blank=True, max_length=500, verbose_name='Примечание')
+    description = models.CharField(blank=True, null=True, max_length=2000, verbose_name='Описание')
+    note = models.CharField(blank=True, null=True, max_length=500, verbose_name='Примечание')
     mileage = models.IntegerField(blank=True, null=True, verbose_name='Пробег')
     price = models.IntegerField(blank=True, null=True, verbose_name='Стоимость')
     date = models.DateField(blank=True)
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + ', ' + self.car_id
+
+    class Meta:
+        verbose_name = 'Ремонт'
+        verbose_name_plural = 'Ремонты'
