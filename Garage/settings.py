118b8a8d992c8413cdbdf28693b416b46f3b8d09
OlgaLyalyueva <olgalyalyueva@gmail.com>
2021-05-13 08:38:08 +0300

    Change settings for Hosts

--- Garage/settings.py
+++ Garage/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-jgppt%-0%trj#-hrn*w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
