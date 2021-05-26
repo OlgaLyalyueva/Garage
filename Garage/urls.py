b6f684d7546dc3bc4d075e5691dfb6b686a0b548
OlgaLyalyueva <olgalyalyueva@gmail.com>
2021-05-26 11:30:16 +0300

    Fix urls

--- Garage/urls.py
+++ Garage/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path, include
 from django.contrib import admin
 
-from views import view
+from views import car_views, insurance_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('cars/', view.get_cars, name='cars'),
-    path('car/<int:car_id>', view.get_car, name='car'),
-    path('add_car/', view.add_car, name='add_car'),
-    path('update_car/<int:car_id>', view.update_car, name='update_car'),
-    path('delete_car/<int:car_id>', view.delete_car, name='delete_car'),
+    path('cars/', car_views.get_cars, name='cars'),
+    path('car/<int:car_id>', car_views.get_car, name='car'),
+    path('add_car/', car_views.add_car, name='add_car'),
+    path('update_car/<int:car_id>', car_views.update_car, name='update_car'),
+    path('delete_car/<int:car_id>', car_views.delete_car, name='delete_car'),
 
-    path('insurances/', view.get_insurances, name='insurances'),
+    path('insurances/', insurance_views.get_insurances, name='insurances'),
 
     path('accounts/', include('django.contrib.auth.urls')),
 
