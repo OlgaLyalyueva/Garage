f5adaaa2e34acb1c5a1c39fccd30d96dd2b2be21
OlgaLyalyueva <olgalyalyueva@gmail.com>
2021-10-07 11:45:44 +0300

    Add settings for run postgres

--- Garage/settings.py
+++ Garage/settings.py
@@ -90,6 +90,17 @@ DATABASES = {
     }
 }
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'garage',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
