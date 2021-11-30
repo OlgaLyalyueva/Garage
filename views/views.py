2a6b5aaf86c096317365e88156ea3828f61b0b2c
OlgaLyalyueva <olgalyalyueva@gmail.com>
2021-11-30 13:07:08 +0200

    Save one image to the car_profile.

--- views/views.py
+++ views/views.py
@@ -12,15 +12,25 @@ def about(request):
     return render(request, 'Garage/about_us.html')
 
 
+def add_image_to_model(request, car):
+    car_photo = CarPhoto(
+        image=request.FILES['image'],
+        car=car
+    )
+    return car_photo
+
+
 def upload_photo(request, car_id):
-    car = get_object_or_404(Car, id=car_id)
+    car = get_object_or_404(Car, id=car_id, user_id=request.user.id)
     if request.method == 'POST':
         form = UploadCarPhoto(request.POST, request.FILES)
         if form.is_valid():
-            car_photo = CarPhoto(
-                image=request.FILES['image'],
-                car=car
-            )
+            try:
+                car_photo = CarPhoto.objects.get(car_id=car_id)
+                car_photo.delete()
+                car_photo = add_image_to_model(request, car)
+            except CarPhoto.DoesNotExist:
+                car_photo = add_image_to_model(request, car)
             car_photo.save()
             return redirect(f'car/{{car.id}}/')
     else:
